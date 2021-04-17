50d7a0ee78d38b4b3ee787052325559ef70de1ff
Natasha Grace <nertasha@outlook.com>
2021-04-17 18:05:26 +0700

    Week 3 Updates

---
+++ Week3/js/map.js
@@ -0,0 +1,59 @@
+var map = L.map('map').setView([1.3521,103.8198], 3);
+
+        let cities = ['Singapore','Bali','Los Angeles','Seoul','Tokyo','New York City']
+
+        let data = [
+	{
+		'title':'SINGAPORE, 2017',
+		'description': 'The river by Clarke Quay, <br> near where I lived for the first 18 years of my life.',
+		'lat': 1.3521,
+		'lon': 103.8198,
+        'img': "https://raw.githubusercontent.com/natgrace/DH151/1804339e9e59c6ae6a0e572730e58053798bd521/Week%202/singapore.jpg",
+	},
+	{
+		'title':'BALI, 2019',
+		'description': 'A rare shot of an empty beach in pre-corona Bali <br> that I took during a family vacation. <br> The first city people think of when I mention my home country. A 9 hour drive from Surabaya, <br> where I am right now!',
+		'lat': 8.3405,
+		'lon': 115.0920,
+        'img': 'https://raw.githubusercontent.com/natgrace/DH151/1804339e9e59c6ae6a0e572730e58053798bd521/Week%202/bali.jpg'
+	},
+	{
+		'title':'LOS ANGELES, 2019',
+		'description': 'Santa Monica Pier, one of the first places I visited <br> when I moved to LA for college. <br> Just a short bus ride away from my community college.',
+		'lat': 34.0522,
+		'lon': -118.2437,
+        'img': "https://raw.githubusercontent.com/natgrace/DH151/1804339e9e59c6ae6a0e572730e58053798bd521/Week%202/la.jpg"
+	},
+	{
+		'title':'SEOUL, 2019',
+		'description': 'My third time visiting South Korea. <br> Also, unknowingly to me then, my last vacation <br> for a while. ',
+		'lat': 37.5665,
+		'lon': 126.9780,
+        'img': "https://raw.githubusercontent.com/natgrace/DH151/1804339e9e59c6ae6a0e572730e58053798bd521/Week%202/seoul.jpg"
+	},
+	{
+		'title':'TOKYO, 2018',
+		'description': 'My first time traveling without my family <br> and my first time to Japan! <br> Also, my favorite picture of the six.',
+		'lat': 35.6762,
+		'lon': 139.6503,
+        'img': "https://raw.githubusercontent.com/natgrace/DH151/52b4f948a203010d14b1f6d40a52b6799b90a9d2/Week%202/tokyo.jpg"
+	},
+	{
+		'title':'NEW YORK CITY, 2018',
+		'description': 'I flew across the country for a concert. <br> My first trip after moving to LA.',
+		'lat': 40.7128,
+		'lon': -74.0060,
+        'img': "https://raw.githubusercontent.com/natgrace/DH151/1804339e9e59c6ae6a0e572730e58053798bd521/Week%202/nyc.jpg"
+	},
+	]
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        data.forEach(function(item){
+		    var marker = L.marker([item.lat,item.lon]).addTo(map)
+		                .bindPopup("<center> <h1>" + item.title + "</h1>" + item.description + "<br> <p>" + "<img src ='" + item.img + "'/>")
+		                .openPopup()
+    
+}); 
